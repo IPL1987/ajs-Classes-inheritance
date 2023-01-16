b0a5d3de8277ff4837a5a357bfb3dd6a3095b9a4
IPL1987 <gmax_lg@ukr.net>
2023-01-16 13:15:10 +0300

    init commit

---
+++ src/js/daemon.js
@@ -0,0 +1,15 @@
+import Character from './domain.js';
+
+export default class Game {
+  start() {
+    console.log('Я ПОБЕДИЛ!');
+  }
+}
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
