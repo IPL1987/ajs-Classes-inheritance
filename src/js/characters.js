9b8c43d782a92a669de432438cbacf3ed346f3da
IPL1987 <ihorpoznyak@yandex.com>
2023-01-16 14:48:40 +0300

    fix

---
+++ src/js/characters.js
@@ -0,0 +1,29 @@
+export default class Character {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+
+    const  heroes = [
+      'Bowman',
+      'Daemon',
+      'Magician',
+      'Swordsman',
+      'Undead',
+      'Zombie',
+    ];
+
+    if (typeof name === 'string' && name.length > 2 || name.length < 10) {
+      throw new Error('Имя героя должно содердать от 2 до 10 символов');
+    } else {
+      this.name = name;
+    };
+
+    if (Character.heroes.includes(type)) {
+      throw new Error('Выберите одного из доступных : Boweman, Swordsman, Magician, Daemon, Undead, Zombie');
+    } else {
+      this.type = type;
+    };
+  };
+};
